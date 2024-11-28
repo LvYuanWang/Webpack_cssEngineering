41d31ec9c697ffbc4ebb976e0716723247e1fb01
LvYuanWang <1175430934@qq.com>
2024-11-28 20:55:28 +0800

    feat: 使用css-loader及style-loader拆分CSS

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// css-loader
+// const css = require("./assets/banner.css");
+
+// const style = document.createElement("style");
+// style.innerHTML = css.toString();
+// document.head.appendChild(style);
+
+// console.log(css.toString());
+
+// style-loader+css-loader
+require("./assets/banner.css");
